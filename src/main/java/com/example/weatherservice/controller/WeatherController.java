99f6192f7cfa65562618991254d1d710270c3d07
rollo <rollovitch.roll@yandex.ru>
2023-09-10 11:59:10 +0300

    modify weather-service

--- src/main/java/com/example/weatherservice/controller/WeatherController.java
+++ src/main/java/com/example/weatherservice/controller/WeatherController.java
@@ -2,11 +2,15 @@ package com.example.weatherservice.controller;
 
 import com.example.weatherservice.dto.weather.Root;
 import com.example.weatherservice.dto.weather.submodules.Coord;
+import com.example.weatherservice.dto.weather.submodules.Main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,11 +19,15 @@ import java.util.concurrent.TimeUnit;
 @RestController
 public class WeatherController {
     private final String appId;
+    private final String weatherUrl;
     private final RestTemplate restTemplate;
 
     @Autowired
-    public WeatherController(@Value("${appid}") String appId, RestTemplate restTemplate) {
+    public WeatherController(@Value("${appid}") String appId,
+                             @Value("${url.weather}") String weatherUrl,
+                             RestTemplate restTemplate) {
         this.appId = appId;
+        this.weatherUrl = weatherUrl;
         this.restTemplate = restTemplate;
     }
 
@@ -28,10 +36,23 @@ public class WeatherController {
     public Root getWeather(@RequestBody Coord coord) throws InterruptedException {//54.1838   45.1749 - Saransk
         TimeUnit.SECONDS.sleep(5);
         return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?" +
-                "lat=" + coord.getLat() +
-                "&lon=" + coord.getLon() +
-                        "&units=metric&appid=" + appId, Root.class);
+                    "lat=" + coord.getLat() +
+                    "&lon=" + coord.getLon() +
+                            "&units=metric&appid=" + appId, Root.class);
     }
+
+    @GetMapping("/weathers")
+    public Main getWeather(@RequestParam String lat, @RequestParam String lon) {
+        //Есть ли способ сделать красивее?
+        String request = weatherUrl +
+                "?lat=" + lat +
+                "&lon=" + lon +
+                "&units=metric&appid=" + appId;
+        // Main не может быть null по определению
+        return restTemplate.getForObject(request, Root.class).getMain();
+    }
+
+
 
 
 }
